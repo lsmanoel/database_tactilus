55d41af30b27f809c27fca5d2b5ebe53f2639fa9
Lucas Seara Manoel <lucas@lucas.domain.lucas>
2019-08-09 20:23:00 -0300

    first commit

---
+++ python_aux/txt2np.py
@@ -0,0 +1,75 @@
+# Python program to convert a list 
+# of character 
+  
+def list2string(s): 
+  
+    # initialization of string to "" 
+    new = "" 
+  
+    # traverse in the string  
+    for x in s: 
+        new += x  
+  
+    # return string  
+    return new 
+
+# ========================================================================================
+file_path = "./../Session.7.9.2019B.txt"
+
+# ========================================================================================
+frame_id_signature = b'FRAME 1703'
+frame_id_signature_size = 10
+frame_signature_size = 5
+
+print_trigger = False
+frame = []
+matrix_string_list = []
+matrix_np = []
+
+with open(file_path, 'rb') as file:
+    for i, line in enumerate(file.readlines()):
+        
+        if line[:frame_signature_size] == frame_id_signature[:frame_signature_size]:
+            print_trigger = False
+            
+        if line[:frame_id_signature_size] == frame_id_signature[:frame_id_signature_size]:    
+            print_trigger = True
+            
+        if print_trigger is True:
+            frame.append(line)
+
+# ========================================================================================            
+for i, line in enumerate(frame):
+    if i>1 and i<34:
+        matrix_string_list.append(str(line)[2:-1])
+
+# ========================================================================================
+import numpy as np
+
+string_buffer = ""
+string_list = []
+matrix_np = np.zeros(32*32)
+matrix_np_index = 0
+
+for i, line in enumerate(matrix_string_list):
+    for j, cell in enumerate(line):
+        if cell == '\\':
+            string_buffer = list2string(string_list)
+            if string_buffer == "":
+                print("---")
+            else:
+                matrix_np[matrix_np_index] = float(string_buffer)
+                print(float(matrix_np[matrix_np_index]))
+                matrix_np_index =+ 1
+        elif cell =='t' or cell =='r' or cell =='n':
+            string_buffer = ""
+            string_list = [] 
+        else:
+            string_list.append(cell) 
+
+matrix_np.reshape((32, 32))
+print(matrix_np.shape)
+
+# ========================================================================================
+#for i, line in enumerate(matrix_string_list):
+#    print(i, line)
